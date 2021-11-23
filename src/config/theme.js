622b8a4909555f7ef7dfeda1e69e2ac08f0b08d2
David Morais <davidmorais92@gmail.com>
2021-11-23 00:02:01 +0000

    add theme

---
+++ src/config/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    accent: "#00FF60",
+    accentAlt: "#00FF80",
+    secondary: "#24104F",
+    titleGradient: 'linear-gradient(180deg, #000000 0%, #00FF80 100%)',
+    accentGradient: 'linear-gradient(180deg, #00FF80 0%, #24104F 99.97%, #061A28 99.98%, #1C9469 99.99%, #15C284 100%)',
+    white: '#ffffff',
+    black: '#000000'
+  }
+}
