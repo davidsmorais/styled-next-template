5fd27994eadb353b689320037b42e5208c460e9b
David Morais <davidmorais92@gmail.com>
2021-11-23 23:15:56 +0000

    📦 NEW: footer and header items config

---
+++ src/config/footerItems.js
@@ -0,0 +1,29 @@
+const items = [
+  {
+    text: 'home',
+    id: 'home',
+  },
+  {
+    text: 'story',
+    id: 'story',
+  },
+  {
+    text: 'game_features',
+    id: 'features',
+  },
+  {
+    text: 'wiki',
+    id: 'wiki',
+  },
+  {
+    text: 'roadmap',
+    id: 'roadmap',
+  },
+  {
+    text: 'newsletterw',
+    id: 'newsletter',
+  },
+];
+
+const icons = ['facebook', 'twitter', 'youtube', 'discord', 'github'];
+export default { items, icons };
---
+++ src/config/headerItems.js
@@ -0,0 +1,28 @@
+const items = [
+  {
+    text: 'home',
+    id: 'home',
+  },
+  {
+    text: 'story',
+    id: 'story',
+  },
+  {
+    text: 'game_features',
+    id: 'features',
+  },
+  {
+    text: 'wiki',
+    id: 'wiki',
+  },
+  {
+    text: 'roadmap',
+    id: 'roadmap',
+  },
+  {
+    text: 'newsletter',
+    id: 'newsletter',
+  },
+];
+
+export default items;
