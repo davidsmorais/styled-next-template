399a0721a60255f76cb67b149d2ce05cd4029176
David Morais <davidmorais92@gmail.com>
2021-11-23 01:05:38 +0000

    add story content

---
+++ src/content/story.js
@@ -0,0 +1,24 @@
+const storyByChapters = [
+  {
+    title: 'In the beginning',
+    text: 'When time and space were created the Primordial Creator had two twins. Ori is the creator of galaxies, planets and being. And Tiamat, one sets life in motion, which, ultimately leads to unpredictability, chaos, and death.',
+    image: 'zeno',
+  },
+  {
+    title: 'Ori and Tiamat',
+    text: 'The twins were sent to write the history of the universe and are forever racing to the End Time. But Ori is always ahead of Tiamat, because Tiamat cannot destroy what is not created.',
+    image: 'twins',
+  },
+  {
+    title: 'The Age of Men',
+    text: 'Ori and Tiamat left descendants and agents on their creations, in case there ever came a time one of them attacked the other.',
+    image: 'descendants',
+  },
+  {
+    title: 'Epoch Rifts',
+    text: "During the Age of Men, Tiamat opened multiple Epoch Rifts. Aware of this, Ori sent his own agents through the timelines to cleanse the corruption and interference of Tiamat's acolytes, these are the Riftwalkers.",
+    image: 'rifts',
+  },
+];
+
+export default storyByChapters;
